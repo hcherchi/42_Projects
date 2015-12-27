c49bb163e658108cbcf4cbed7af376cc0818bc35
Hugo Cherchi <Hugo@MacBook-Air-de-Hugo.local>
2015-12-27 13:00:15 +0100

    maj

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,15 +6,14 @@
 /*   By: hcherchi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/05 18:40:55 by hcherchi          #+#    #+#             */
-/*   Updated: 2015/12/22 16:46:44 by hcherchi         ###   ########.fr       */
+/*   Updated: 2015/12/19 15:23:35 by hcherchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <fcntl.h>
 # include "libft.h"
-# define BUFF_SIZE 8
+# define BUFF_SIZE 2
 
 int		get_next_line(int const fd, char **line);
 #endif
