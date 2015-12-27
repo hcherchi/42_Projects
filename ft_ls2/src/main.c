cef7c70500f9e575a6fb208e10e8eb90ab09aa84
Bruno GANTELMI <bgantelm@e1r10p12.42.fr>
2015-12-27 16:57:16 +0100

    ajoutft_ls

---
+++ ft_ls2/src/main.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgantelm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/23 11:50:55 by bgantelm          #+#    #+#             */
+/*   Updated: 2015/12/23 13:08:50 by bgantelm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void	ft_ls_files(t_args *args)
+{
+	if (!args->files && !args->dirs && args->error == 0)
+		recursive(".", args->opt);
+	if (args->files)
+	{
+		args->nofile = 0;
+		choose_sort(&(args->files), args->opt);
+		print_files(args->files, args->opt);
+		if (args->dirs)
+			ft_putchar('\n');
+	}
+}
+
+void	ft_ls_dirs(t_args *args)
+{
+	if (args->dirs)
+	{
+		if (args->dirs->next == NULL)
+			args->onlyonedir = 1;
+		choose_sort(&(args->dirs), args->opt);
+		while (args->dirs)
+		{
+			if (!(args->onlyonedir && args->nofile) || args->error)
+			{
+				ft_putstr(args->dirs->data->path);
+				ft_putendl(":");
+			}
+			recursive(args->dirs->data->path, args->opt);
+			args->dirs = args->dirs->next;
+			if (args->dirs != NULL)
+				ft_putchar('\n');
+		}
+	}
+}
+
+int		main(int ac, char **av)
+{
+	int		i;
+	t_args	*args;
+
+	args = malloc(sizeof(*args));
+	i = 1;
+	init_args(args);
+	while (i < ac && av[i][0] == '-')
+	{
+		check_options(av[i], args);
+		i++;
+	}
+	if (args->opt->h)
+	{
+		hoption();
+		return (0);
+	}
+	while (i < ac)
+	{
+		check_file(av[i], args);
+		i++;
+	}
+	ft_ls_files(args);
+	ft_ls_dirs(args);
+	argsdel(args);
+	return (0);
+}
---
+++ ft_ls2/src/sort_second.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_second.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgantelm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/23 11:11:45 by bgantelm          #+#    #+#             */
+/*   Updated: 2015/12/23 13:18:24 by bgantelm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void	reverse(t_file **l_files)
+{
+	t_file	*inv;
+	t_file	*cur;
+
+	cur = *l_files;
+	inv = NULL;
+	if (!*l_files)
+		return ;
+	else
+	{
+		while ((*l_files)->next)
+		{
+			while (cur->next->next)
+			{
+				cur = cur->next;
+			}
+			add_file(&inv, cur->next);
+			cur->next = NULL;
+			cur = *l_files;
+		}
+		add_file(&inv, *l_files);
+	}
+	*l_files = inv;
+}
+
+void	choose_sort(t_file **l_files, t_option *opt)
+{
+	if (opt->f == 0)
+	{
+		sort_ascii(l_files);
+		if (opt->t)
+			sort_time(l_files);
+		if (opt->r)
+			reverse(l_files);
+	}
+}
+
+void	putpath(t_file *l_files)
+{
+	t_file	*cur;
+
+	cur = l_files;
+	ft_putchar('\n');
+	ft_putstr(cur->data->path);
+	ft_putstr(":\n");
+}
+
+void	putlink(t_file *l_files)
+{
+	t_file	*cur;
+
+	cur = l_files;
+	ft_putstr(" -> ");
+	ft_putstr(cur->data->namelk);
+}
+
+void	hoption(void)
+{
+	ft_putstr("--R afficher récursivement les sous-répertoires.\n");
+	ft_putstr("--l utiliser un format d’affichage long.\n");
+	ft_putstr("--a inclure les entrées débutant par « . ».\n");
+	ft_putstr("--r inverser l'ordre du tri.\n");
+	ft_putstr("--t trier selon la date de modification.\n");
+	ft_putstr("--p ajouter l’indicateur « / » aux répertoires.\n");
+	ft_putstr("--f ne pas trier, activer les options -aU et");
+	ft_putstr(" désactiver les  options -ls --color.\n");
+	ft_putstr("--g identique à -l mais sans afficher le propriétaire.\n");
+	ft_putstr("--h aide pour les options.\n");
+}
