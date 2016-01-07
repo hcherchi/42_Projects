facfe2fe1a8e6064bd1f6eacdcf13f929b5dc646
Hugo CHERCHI <hcherchi@e1r9p3.42.fr>
2016-01-07 16:54:35 +0100

    yo

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -9,26 +9,21 @@
 
 void        ft_setenv(char ***myenv, char *av1, char *av2);
 char        **add_tab(char **tab, char *elem);
-
 void        ft_unsetenv(char ***myenv, char **av);
 char        **remove_tab(char **tab, char *elem);
-
 void        ft_cd(char ***myenv, char **av);
 char        *cdsplit(char *cdsplit, char *home);
 void        cd_error(char *newpath, char **home);
-
 void        execute(char **myenv, char **av, char *good_path);
 void        set_commands(char ***myenv, char **av);
 void        run_command(char *command, char ***myenv, char **av);
 void        select_command(char **av, char ***myenv);
-
 char        *get_elem(char **env, char *elem);
 int         select_path(char *paths, char *elem, char **good_path);
 char        *get_name(char *line);
 void        lvlup(char ***myenv);
 char        *strchange(char *str, char c, char *str2);
 int         permission(char *path);
-
 char        *add_path(char *p, char *n, char c);
 char        *del_path(char *s);
 void        ft_puttab(char **tab);
