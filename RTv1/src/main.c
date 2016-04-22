e6de2202df052a742e22f555f451db2fd06f9184
Bruno GANTELMI <bgantelm@e1r6p17.42.fr>
2016-04-22 16:47:40 +0200

    SF du repeertory

--- RTv1/src/main.c
+++ RTv1/src/main.c
@@ -8,6 +8,11 @@ void    init_menu(t_tool *tools)
     tools->m->bg = malloc(sizeof(t_image));
     tools->m->bg->texture = ft_strdup("textures/blue_background.xpm");
     tools->m->bg->mlx_img = mlx_xpm_file_to_image(tools->mlx_ptr, tools->m->bg->texture, &tools->m->bg->width, &tools->m->bg->height);
+    if (tools->m->bg->mlx_img == NULL)
+    {
+        ft_putendl("Error, you are not allowed to lauch RT from this directory.");
+        exit(0);
+    }
     tools->m->bg->data = mlx_get_data_addr(tools->m->bg->mlx_img, &tools->m->bg->bpp, &tools->m->bg->size_line, &tools->m->bg->endian);
     tools->m->bg->screen = NULL;
     tools->error = 0;
