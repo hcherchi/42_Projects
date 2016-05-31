b0cb837279fd7f9eaffabd8204a9bb380b33bc19
Vincent NGUYEN <vnguyen@e1r9p17.42.fr>
2016-05-31 19:23:07 +0200

    norme

---
+++ RTv1/src/color2.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnguyen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/31 19:22:02 by vnguyen           #+#    #+#             */
+/*   Updated: 2016/05/31 19:22:25 by vnguyen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <rtv1.h>
+
+void        normalize_color(t_color *final_color)
+{
+	if (final_color->r < 0)
+		final_color->r = 0;
+	if (final_color->b < 0)
+		final_color->b = 0;
+	if (final_color->g < 0)
+		final_color->g = 0;
+	if (final_color->r > 255)
+		final_color->r = 255;
+	if (final_color->b > 255)
+		final_color->b = 255;
+	if (final_color->g > 255)
+		final_color->g = 255;
+}
