fc98351f4c6c507c717e52bca6d974d033a38934
Floren HENRI <fhenri@e1r10p18.42.fr>
2016-05-31 19:11:09 +0200

    up norme

--- RTv1/src/parser.c
+++ RTv1/src/parser.c
@@ -1,10 +1,22 @@
 #include <rtv1.h>
 
+void	parse_camera_suite(t_tool *tools, char **split)
+{
+	if (str_digit(split[1]) || str_digit(split[2]))
+		ft_error(2, tools);
+	if (ft_atof(split[1]) > 2000 || ft_atof(split[2]) > 1300)
+		ft_error(13, tools);
+	if (ft_atoi(split[1]) < 0 || ft_atoi(split[2]) < 0)
+		ft_error(8, tools);
+	tools->rt->x_res = ft_atoi(split[1]);
+	tools->rt->y_res = ft_atoi(split[2]);
+}
+
 void	parse_camera(t_tool *tools, int fd)
 {
 	char	**split;
 	char	*line;
-    
+
 	while (get_next_line(fd, &line) > 0 && ft_strcmp(line, "}"))
 	{
 		split = ft_strsplit(line, ' ');
@@ -13,51 +25,61 @@ void	parse_camera(t_tool *tools, int fd)
 		else if (ft_strstr(line, "res:"))
 		{
 			if (ft_tablen(split) == 3)
-			{
-				if (str_digit(split[1]) || str_digit(split[2]))
-					ft_error(2, tools);
-				if(ft_atof(split[1]) > 2000 || ft_atof(split[2]) > 1300)
-					ft_error(13, tools);
-				if (ft_atoi(split[1]) < 0 || ft_atoi(split[2]) < 0)
-					ft_error(8, tools);
-				tools->rt->x_res = ft_atoi(split[1]);
-				tools->rt->y_res = ft_atoi(split[2]);
-			}
+				parse_camera_suite(tools, split);
 			else
 				ft_error(7, tools);
 		}
-        else
-            parse_camera2(line, tools, split);
-        clean_tab(split);
-        free(line);
+		else
+			parse_camera2(line, tools, split);
+		clean_tab(split);
+		free(line);
 	}
 }
 
-void    parse_camera2(char *line, t_tool *tools, char **split)
+void	parse_camera2(char *line, t_tool *tools, char **split)
 {
-    if (ft_strstr(line, "lumamb:"))
-    {
-        if (ft_tablen(split) == 2)
-	        tools->rt->lumamb = ft_atof(split[1]);
-	    else
-		    ft_error(7, tools);
-    }
-    else if (ft_strstr(line, "vect:"))
-    {
-            tools->rt->vect = fill_pos(split, tools);
-    }
-    else if (ft_strstr(line, "skybox:"))
-    {
-        if (ft_tablen(split) == 2)
-    	    tools->rt->sky = fill_texture(split[1], tools);
-    	else
-    	    ft_error(7, tools);
-    }
-    else if (ft_strcmp(line, "{"))
-        ft_error(1, tools);
+	if (ft_strstr(line, "lumamb:"))
+	{
+		if (ft_tablen(split) == 2)
+			tools->rt->lumamb = ft_atof(split[1]);
+		else
+			ft_error(7, tools);
+	}
+	else if (ft_strstr(line, "vect:"))
+	{
+		tools->rt->vect = fill_pos(split, tools);
+	}
+	else if (ft_strstr(line, "skybox:"))
+	{
+		if (ft_tablen(split) == 2)
+			tools->rt->sky = fill_texture(split[1], tools);
+		else
+			ft_error(7, tools);
+	}
+	else if (ft_strcmp(line, "{"))
+		ft_error(1, tools);
 }
 
-
+int		roud_boucle(t_tool *tools, int c, int fd, char *line)
+{
+	if (!ft_strcmp(line, "camera"))
+	{
+		c++;
+		free(line);
+		parse_camera(tools, fd);
+	}
+	else if (!ft_strcmp(line, "light"))
+	{
+		free(line);
+		parse_light(tools, fd);
+	}
+	else if (!ft_strcmp(line, "object"))
+	{
+		free(line);
+		parse_object(tools, fd);
+	}
+	return (c);
+}
 
 void	parser(int fd, t_tool *tools)
 {
@@ -66,29 +88,14 @@ void	parser(int fd, t_tool *tools)
 	int		ret;
 
 	c = 0;
-    tools->rt = malloc(sizeof(t_rt));
-    tools->rt->l_objects = NULL;
-    tools->rt->l_lights = NULL;
-    tools->rt->mlx_win = NULL;
-    init_camera(tools);
+	tools->rt = malloc(sizeof(t_rt));
+	tools->rt->l_objects = NULL;
+	tools->rt->l_lights = NULL;
+	tools->rt->mlx_win = NULL;
+	init_camera(tools);
 	while ((ret = get_next_line(fd, &line)) > 0)
 	{
-		if (!ft_strcmp(line, "camera"))
-		{
-			c++;
-            free(line);
-            parse_camera(tools, fd);
-		}
-		else if (!ft_strcmp(line, "light"))
-        {
-            free(line);
-			parse_light(tools, fd);
-        }
-		else if (!ft_strcmp(line, "object"))
-        {
-            free(line);
-			parse_object(tools, fd);
-        }
+		c = roud_boucle(tools, c, fd, line);
 	}
 	if (ret == -1)
 		ft_error(10, tools);
--- RTv1/src/parser2.c
+++ RTv1/src/parser2.c
@@ -11,73 +11,73 @@ void	parse_light(t_tool *tools, int fd)
 	while (get_next_line(fd, &line) > 0 && !ft_strstr(line, "}"))
 	{
 		split = ft_strsplit(line, ' ');
-        if (ft_strstr(line, "pos:"))
-        {
-            free(light->o);
-            light->o = fill_pos(split, tools);
-        }        
-        else
-            parse_light2(line, light, split, tools);
-        clean_tab(split);
-        free(line);
+		if (ft_strstr(line, "pos:"))
+		{
+			free(light->o);
+			light->o = fill_pos(split, tools);
+		}
+		else
+			parse_light2(line, light, split, tools);
+		clean_tab(split);
+		free(line);
 	}
-    vectornorm(light->d);
+	vectornorm(light->d);
 	add_light(&tools->rt->l_lights, light);
 }
 
-void    parse_light2(char *line, t_light *light, char **split, t_tool *tools)
+void	parse_light2(char *line, t_light *light, char **split, t_tool *tools)
 {
-    if (ft_strstr(line, "dir:"))
-    {
-        free(light->d);
-        light->d = fill_pos(split, tools);
-    }
-    else if (ft_strstr(line, "color:"))
-    {
-        free(light->color);
-        light->color = fill_color(split, tools);
-    }
-    else if (ft_strstr(line, "lumdiff:"))
-    {
-        if (ft_tablen(split) == 2)
-        {
-            if (str_digit(split[1]))
-                ft_error(2, tools);
-            light->lumdiff = ft_atof(split[1]);
-        }
-        else
-            ft_error(7, tools);
-    }
-    else
-        parse_light3(line, light, split, tools);
+	if (ft_strstr(line, "dir:"))
+	{
+		free(light->d);
+		light->d = fill_pos(split, tools);
+	}
+	else if (ft_strstr(line, "color:"))
+	{
+		free(light->color);
+		light->color = fill_color(split, tools);
+	}
+	else if (ft_strstr(line, "lumdiff:"))
+	{
+		if (ft_tablen(split) == 2)
+		{
+			if (str_digit(split[1]))
+				ft_error(2, tools);
+			light->lumdiff = ft_atof(split[1]);
+		}
+		else
+			ft_error(7, tools);
+	}
+	else
+		parse_light3(line, light, split, tools);
 }
 
-void    parse_light3(char *line, t_light *light, char **split, t_tool *tools)
+void	parse_light3(char *line, t_light *light, char **split, t_tool *tools)
 {
-    if (ft_strstr(line, "h:"))
-    {
-        if (ft_tablen(split) == 2)
-        {
-            if (str_digit(split[1]))
-                ft_error(2, tools);
-            light->h = ft_atof(split[1]);
-        }
-        else
-            ft_error(7, tools);
-    }
-    else if (ft_strstr(line, "angle:"))
-    {
-        if (ft_tablen(split) == 2)
-        {
-            if (str_digit(split[1]))
-                ft_error(2, tools);
-            light->angle = ft_atof(split[1]);
-        }
-        else
-            ft_error(7, tools);
-    }
-    else if (ft_strstr(line, "type:"))
-        light->type = light_type(split, tools);
-    else if (ft_strcmp(line, "{"))
-        ft_error(3, tools);
+	if (ft_strstr(line, "h:"))
+	{
+		if (ft_tablen(split) == 2)
+		{
+			if (str_digit(split[1]))
+				ft_error(2, tools);
+			light->h = ft_atof(split[1]);
+		}
+		else
+			ft_error(7, tools);
+	}
+	else if (ft_strstr(line, "angle:"))
+	{
+		if (ft_tablen(split) == 2)
+		{
+			if (str_digit(split[1]))
+				ft_error(2, tools);
+			light->angle = ft_atof(split[1]);
+		}
+		else
+			ft_error(7, tools);
+	}
+	else if (ft_strstr(line, "type:"))
+		light->type = light_type(split, tools);
+	else if (ft_strcmp(line, "{"))
+		ft_error(3, tools);
 }
