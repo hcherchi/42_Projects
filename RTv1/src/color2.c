f65d9fb38396dac90e231e1d32c51e2dff3635f1
Vincent NGUYEN <vnguyen@e1r9p17.42.fr>
2016-05-31 20:05:44 +0200

    spaces

--- RTv1/src/color2.c
+++ RTv1/src/color2.c
@@ -6,13 +6,13 @@
 /*   By: vnguyen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/31 19:22:02 by vnguyen           #+#    #+#             */
-/*   Updated: 2016/05/31 19:22:25 by vnguyen          ###   ########.fr       */
+/*   Updated: 2016/05/31 20:05:30 by vnguyen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <rtv1.h>
 
-void        normalize_color(t_color *final_color)
+void	normalize_color(t_color *final_color)
 {
 	if (final_color->r < 0)
 		final_color->r = 0;
