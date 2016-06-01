3f083fbbf172976f967ae7b5de0dd2a5e3d67546
Floren HENRI <fhenri@e1r10p20.42.fr>
2016-06-01 10:43:33 +0200

    merge

--- RTv1/src/clean3.c
+++ RTv1/src/clean3.c
@@ -6,11 +6,21 @@
 /*   By: vnguyen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/31 19:51:58 by vnguyen           #+#    #+#             */
-/*   Updated: 2016/05/31 19:53:04 by vnguyen          ###   ########.fr       */
+/*   Updated: 2016/06/01 10:42:04 by fhenri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <rtv1.h>
+
+void	texture_suite(t_double *deux, t_object *object)
+{
+	deux->tmp = deux->y / object->texture->height;
+	deux->div = floor(deux->y / object->texture->height);
+	deux->y = (deux->tmp - deux->div) * object->texture->height;
+	deux->tmp = deux->x / object->texture->width;
+	deux->div = floor(deux->tmp);
+	deux->x = (deux->tmp - deux->div) * object->texture->height;
+}
 
 void	clean_tab(char **tab)
 {
