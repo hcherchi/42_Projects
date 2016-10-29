ccf1f2250845f2b8c72994af6e90f0a4f2a8e9b2
Hugo CHERCHI <hugo_cherchi@hotmail.fr>
2016-10-29 17:09:23 +0200

    [UPD]: upgraded utils func

--- Wolf3D/includes/wolf3d.h
+++ Wolf3D/includes/wolf3d.h
@@ -19,21 +19,21 @@
 # include <fcntl.h>
 # include <stdio.h>
 
-#define degreesToRadians(angleDegrees) (angleDegrees * M_PI / (double)180)
+#define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180)
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 #define LIM 1000
-#define NORTH 0xFFDAB9
-#define SOUTH 0xD2691E
-#define EAST 0xFF0000
-#define WEST 0x00FF00
-#define SKY 0x87CEEB
-#define GROUND 0xADFF2F
-#define E 0.000001
+#define NORTH 0x8B4513
+#define SOUTH 0xCD853F
+#define EAST 0xA0522D
+#define WEST 0xD2691E
+#define SKY 0x48D1CC
+#define GROUND 0x6B8E23
+#define E 0.0001
 
 typedef struct	s_point
 {
-	double		x;
-	double		y;
+	float		x;
+	float		y;
 }								t_point;
 
 typedef struct	s_tool
@@ -44,15 +44,15 @@ typedef struct	s_tool
 	int		color;
 	int		**grid;
 	t_point *pos;
-	double		angle;
+	float		angle;
 	int			cubeSize;
 	int		FOV;
 	int		screenWidth;
 	int		screenHeight;
 	int		centerX;
 	int		centerY;
-	double		dist;
-	double		incAngle;
+	float		dist;
+	float		incAngle;
 	void *mlx_ptr;
 	void *mlx_win;
 	void *mlx_img;
@@ -61,23 +61,23 @@ typedef struct	s_tool
 	int			size_line;
 }				t_tool;
 
-t_point *getVertical(t_tool *t, double rayAngle);
-t_point *getFirstPointVertical(t_tool *t, double rayAngle);
+t_point *getVertical(t_tool *t, float rayAngle);
+t_point *getFirstPointVertical(t_tool *t, float rayAngle);
 t_point *getNextPointVertical(t_tool *t, t_point *curPoint, t_point *inc);
 
-t_point *getHorizontal(t_tool *t, double rayAngle);
-t_point *getFirstPointHorizontal(t_tool *t, double rayAngle);
+t_point *getHorizontal(t_tool *t, float rayAngle);
+t_point *getFirstPointHorizontal(t_tool *t, float rayAngle);
 t_point *getNextPointHorizontal(t_tool *t, t_point *curPoint, t_point *inc);
 
 int getWallHeight(int col, t_tool *t);
 
-double getDist(t_point *wall, t_tool *t);
-int   isWall(t_point *intersection, t_tool *t);
-int   insideMap(t_point *intersection, t_tool *t);
+float getDist(t_point *wall, t_tool *t);
+int   isWall(t_point *intersection, t_tool *t, float rayAngle);
+int   insideMap(t_point *intersection, t_tool *t, float rayAngle);
 void	pixel_put_to_image(int color, t_tool *t, int x, int y);
-double adjustAngle(double angle, double inc);
-int   isUpPart(double angle);
-int isRightPart(double angle);
+float adjustAngle(float angle, float inc);
+int   isUpPart(float angle);
+int isRightPart(float angle);
 
 void drawCol(int wallHeight, int col, t_tool *t);
 void launch(t_tool *tools);
--- Wolf3D/src/horizontal.c
+++ Wolf3D/src/horizontal.c
@@ -12,7 +12,7 @@
 
 #include "wolf3d.h"
 
-t_point *getHorizontal(t_tool *t, double rayAngle)
+t_point *getHorizontal(t_tool *t, float rayAngle)
 {
   t_point *inc;
   t_point *curPoint;
@@ -21,19 +21,19 @@ t_point *getHorizontal(t_tool *t, double rayAngle)
   curPoint = getFirstPointHorizontal(t, rayAngle);
   inc->y = isUpPart(rayAngle) ? - t->cubeSize : + t->cubeSize;
   inc->x = - inc->y / tan(degreesToRadians(rayAngle));
-  while (insideMap(curPoint, t) && !isWall(curPoint, t))
+  while (insideMap(curPoint, t, rayAngle) && !isWall(curPoint, t, rayAngle))
     curPoint = getNextPointHorizontal(t, curPoint, inc);
   return curPoint;
 }
 
-t_point *getFirstPointHorizontal(t_tool *t, double rayAngle)
+t_point *getFirstPointHorizontal(t_tool *t, float rayAngle)
 {
   t_point *point;
 
   point = malloc(sizeof(*point));
 
   if (isUpPart(rayAngle))
-    point->y = floor(t->pos->y / t->cubeSize) * t->cubeSize - E;
+    point->y = floor(t->pos->y / t->cubeSize) * t->cubeSize;
   else
     point->y = floor(t->pos->y / t->cubeSize) * t->cubeSize + t->cubeSize;
   point->x = t->pos->x + (t->pos->y - point->y) / tan(degreesToRadians(rayAngle));
@@ -43,7 +43,7 @@ t_point *getFirstPointHorizontal(t_tool *t, double rayAngle)
 t_point *getNextPointHorizontal(t_tool *t, t_point *curPoint, t_point *inc)
 {
   t_point *nextPoint;
-  double dist;
+  float dist;
 
   nextPoint = malloc(sizeof(*nextPoint));
   nextPoint->x = curPoint->x + inc->x;
