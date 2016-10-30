706c4e901ea294cffb8daa775b7b5513f26b6c36
Floren HENRI <fhenri@e1r10p15.42.fr>
2016-10-30 15:40:51 +0100

    [UPD] login

--- camagru/components/create_account.php
+++ camagru/components/create_account.php
@@ -1,10 +1,24 @@
 <?php
 session_start();
+require('./../config/setup.php');
+if ($_POST['submit'] === "register_button") {
+	if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])) {
+		echo "whech alors";
+		$username = $db->quote($_POST['username']);
+		$email = $db->quote($_POST['email']);
+		$password = $db->quote(sha1($_POST['password']));
+		$select = $db->query("SELECT * FROM User WHERE username=$username OR email=$email");
 
-if ($_POST['submit'] !== "M'inscrire") {
-echo "hello";
+		if ($select->rowCount() == 0) {
+			$db->query("INSERT INTO User SET username=$username, password=$password, email=$email");
+			mail( $_POST['email'] , "new count" , "Welcom to you ".$_POST['username'].", you successefully regiter to Camagru. lets start !!" );
+		}
+
+	} else {
+		echo "Messing Information";
+	}
+} else {
+	echo "503 bag get a way Forbiden";
 }
-
-echo "hello or du if";
 ?>
 
