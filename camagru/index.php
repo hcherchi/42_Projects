f81900149d691eaf525f64f5f8920cccd52673bc
Hugo CHERCHI <hugo_cherchi@hotmail.fr>
2016-11-01 20:34:50 +0100

    [UPD]: start css page camera

--- camagru/index.php
+++ camagru/index.php
@@ -2,15 +2,23 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html class="h-100 w-100">
   <head>
     <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <link rel="stylesheet" type="text/css" href="./css/flex.css">
+    <link rel="stylesheet" type="text/css" href="./css/size.css">
+    <link rel="stylesheet" type="text/css" href="./css/margin.css">
+    <link rel="stylesheet" type="text/css" href="./css/padding.css">
+    <link rel="stylesheet" type="text/css" href="./css/bg.css">
+    <link rel="stylesheet" type="text/css" href="./css/font.css">
     <meta charset="UTF-8">
     <title>I <3 Camagru</title>
   </head>
-  <body>
-    <?PHP include("components/header.html"); ?>
-    <?PHP include("components/sign.html"); ?>
-    <?PHP include("components/footer.html"); ?>
+  <body class="flex-v w-100 h-100 items-stretch">
+    <?PHP include("html/header.html"); ?>
+    <div class="self-flex flex-v justify-center items-center">
+      <?PHP include("html/cam.html"); ?>
+    </div>
+    <?PHP include("html/footer.html"); ?>
   </body>
 </html>
