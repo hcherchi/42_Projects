551298a4a21fe8dfe08a2084149d129350620536
Floren Henri <fhenri@student.42.fr>
2017-01-11 15:54:59 +0100

    [ADD] attribute zero && space && plus

--- printf/main.c
+++ printf/main.c
@@ -3,6 +3,11 @@
 
 int main(void)
 {
-    ft_printf("salut %c %s %d trop trop trop\n", 'c', "Hugo", 10);
+  int t = 421234;
+  //  ft_printf("salut %c %s %d trop trop trop\n", 'c', "Hugo", 10);
+  printf("hello no flag %c\n", t );
+  printf("hello flag - = %20d\n", t );
+  printf("hello flag + = %+d\n", t );
+  printf("hello flag 0 = %+043d\n", t );
     return (0);
 }
