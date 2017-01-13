5f00bce77020cab48fa03e3595c3b245968bbb81
Hugo CHERCHI <hcherchi@e1r4p14.42.fr>
2017-01-13 10:35:55 +0100

    [UPD]: less lines on attributes handling

--- printfv2/src/attributs.c
+++ printfv2/src/attributs.c
@@ -12,14 +12,11 @@ char *add_prefix(t_format *format, char *buf)
     else
       return (ft_strcat("0", buf));
   }
-  else if (format->plus)
+  if (buf[0] != '-' && ft_strchr("di", format->type))
   {
-    if (buf[0] != '-')
+    if (format->plus)
       return (ft_strcat("+", buf));
-  }
-  else if (format->space)
-  {
-    if (buf[0] != '-')
+    else if (format->space)
       return (ft_strcat(" ", buf));
   }
   return ft_strdup(buf);
@@ -34,15 +31,9 @@ char *ft_attribute(t_format *format, char *buf)
 
   prefixedbuf = add_prefix(format, buf);
   tofill = format->width - ft_strlen(prefixedbuf);
+  filler = (format->zero) ? '0' : ' ';
   if (tofill <= 0)
     return (prefixedbuf);
-  if (format->zero)
-    filler = '0';
-  else
-    filler = ' ';
-  if (format->moins)
-    finalbuf = ft_strcat(prefixedbuf, ft_strnew(filler, tofill));
-  else
-    finalbuf = ft_strcat(ft_strnew(filler, tofill), prefixedbuf);
+  finalbuf = (format->moins) ? ft_strcat(prefixedbuf, ft_strnew(filler, tofill)) : ft_strcat(ft_strnew(filler, tofill), prefixedbuf);
   return (finalbuf);
 }
