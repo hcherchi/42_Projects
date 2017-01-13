edfe54bf8cc5c9c8051e3c1fd762ca95eb5648d9
Hugo CHERCHI <hcherchi@e1r4p14.42.fr>
2017-01-13 13:52:10 +0100

    [FIX]

--- printfv2/src/parser.c
+++ printfv2/src/parser.c
@@ -155,8 +155,8 @@ int   handle_convertion(const char *input, va_list ap, int *count)
 
   format = init_format();
   pass = fill_format(input, format);
-  //print_struct(format);
   update_format(format);
+  //print_struct(format);
 
   to_print = choose_convertion(format, ap);
   to_print = ft_attribute(format, to_print);
